a366498997e7c2a723b0d265df886bca939a7080
phamngocquang0072 <phamngocquang0072@gmail.com>
2023-04-28 21:51:05 +0700

    new update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("Galaxy Fighting Lỏ")
 
 #set framerate
 clock = pygame.time.Clock()
@@ -18,7 +18,7 @@ FPS = 60
 
 #define colours
 RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 #define game variables
@@ -80,7 +80,7 @@ def draw_health_bar(health, x, y):
   ratio = health / 100
   pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
   pygame.draw.rect(screen, RED, (x, y, 400, 30))
-  pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
+  pygame.draw.rect(screen, GREEN, (x, y, 400 * ratio, 30))
 
 
 #create two instances of fighters
@@ -99,10 +99,10 @@ while run:
   #show player stats
   draw_health_bar(fighter_1.health, 20, 20)
   draw_health_bar(fighter_2.health, 580, 20)
-  draw_text("P1: " + str(score[0]), score_font, RED, 20, 60)
-  draw_text("P2: " + str(score[1]), score_font, RED, 580, 60)
+  draw_text("1st player: " + str(score[0]), score_font, RED, 20, 60)
+  draw_text("2nd player: " + str(score[1]), score_font, RED, 580, 60)
 
-  #update countdown
+  # update countdown
   if intro_count <= 0:
     #move fighters
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
